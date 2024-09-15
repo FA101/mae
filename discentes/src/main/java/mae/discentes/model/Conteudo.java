835eb3a7c8563bbfe773ffe019c384dd632fff5c
Thiago Peraro <55365235+tperaro@users.noreply.github.com>
2024-09-15 20:21:05 -0300

    tentando subir o banco

--- discentes/src/main/java/mae/discentes/model/Conteudo.java
+++ discentes/src/main/java/mae/discentes/model/Conteudo.java
@@ -1,12 +1,19 @@
 package mae.discentes.model;
 import javax.validation.constraints.NotBlank;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import org.antlr.v4.runtime.misc.NotNull;
-
+@Entity
 public class Conteudo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     @NotBlank
-    Integer upvote;
+    private Integer upvote;
     @NotBlank
-    Integer downvote;
+    private Integer downvote;
 
 }
