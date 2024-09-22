4ab8cb4f9da660091547cde6b758f742ea32ab28
Fillipe <fillipe.albuquerque@discente.ufg.br>
2024-09-22 19:47:06 -0300

    #01 Inicio back docente

---
+++ docente/docente-back/docente-back/src/main/java/Repositories/DocenteRepositorio.java
@@ -0,0 +1,8 @@
+package Repositories;
+
+import Models.Docente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DocenteRepositorio extends JpaRepository<Docente, Long> {
+}
+
