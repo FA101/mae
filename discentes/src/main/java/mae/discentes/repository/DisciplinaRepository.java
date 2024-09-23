705efb7da2ba26a223b2bce4bc34b599287e8a98
Thiago Peraro <55365235+tperaro@users.noreply.github.com>
2024-09-23 20:05:31 -0300

    Adicionando DISCIPLINA, Banco online e preparando pra subir aplicacao no fly.io

---
+++ discentes/src/main/java/mae/discentes/repository/DisciplinaRepository.java
@@ -0,0 +1,15 @@
+package mae.discentes.repository;
+
+import mae.discentes.model.Disciplina;
+import mae.discentes.model.InteresseVerbete;
+import mae.discentes.model.InteresseVerbeteId;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DisciplinaRepository  extends JpaRepository<Disciplina,Long>{
+
+}
+
+
+
+
+
