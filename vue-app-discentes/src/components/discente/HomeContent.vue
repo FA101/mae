8733f97b21b72c3eb38b753c9c8178f21bd629b9
Giodzz <giordanabbucci@gmail.com>
2024-09-23 14:11:09 -0300

    HomeContent corrigido

--- vue-app-discentes/src/components/discente/HomeContent.vue
+++ vue-app-discentes/src/components/discente/HomeContent.vue
@@ -13,12 +13,11 @@
     justify-content: center;
     align-items: center;
     height: 100%;
-    color: #f1f1f1;
   }
   
   .home-content h1 {
     font-size: 4rem;
-    color: rgba(0, 0, 0, 0.1);
+    color: #EBE9E9;
     text-align: center;
   }
   </style>
